a396b2bf32dfd56bac4ad962e350184d2749491e
Fearless <489368600@qq.com>
2020-05-25 19:03:31 +0800

    商城列表搭建

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
-// 箭头函数
 main() => runApp(MyApp());
 
-// 无状态的Widget：内容是确定没有状态的改变
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: ZCHomePage()
+      home: ZCHomePage(),
     );
   }
 }
@@ -18,49 +15,72 @@ class ZCHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("同意和解协议"),
-        ),
-        body: ZCHomeContentBody()
+      appBar: AppBar(
+        title: Text("首页商城",),
+      ),
+      body: ZCHomeContent()
+    );
+  }
+}
+
+class ZCHomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ZCHomeProductItem("Apple1", "Macbook1", "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2196657174,1994863858&fm=26&gp=0.jpg"),
+        SizedBox(height: 5),
+        ZCHomeProductItem("Apple2", "Macbook2", "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3368267992,623076164&fm=26&gp=0.jpg"),
+        SizedBox(height: 5),
+        ZCHomeProductItem("Apple3", "Macbook4", "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1422590462,2950091126&fm=26&gp=0.jpg"),
+      ],
     );
   }
 }
 
 
-/// flag状态
-/// Stateful不能定义状态：创建一个单独的类，这个类负责维护状态
-class ZCHomeContentBody extends StatefulWidget {
-  @override
-  /// 抽象方法 必须实现方法 重写方法去实现
-  _ZCHomeContentBodyState createState() => _ZCHomeContentBodyState();
-}
+class ZCHomeProductItem extends StatelessWidget {
 
-/// State去构建Widget
-class _ZCHomeContentBodyState extends State<ZCHomeContentBody> {
-  
-  var flag = true;
-  
+  final String title;
+  final String desc;
+  final String imageURL;
+
+  final textStyle1 = TextStyle(fontSize: 25, color: Colors.orange);
+  final textStyle2 = TextStyle(fontSize: 20, color: Colors.red);
+
+  /// 构造函数
+  ZCHomeProductItem(this.title, this.desc, this.imageURL);
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 5, /// 设置边框的宽度
+          color: Colors.orange, /// 设置监控的颜色
+        ),
+      ) ,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,  /// 主轴
+        crossAxisAlignment: CrossAxisAlignment.start, /// 交叉轴
         children: <Widget>[
-          Checkbox(
-              value: flag,
-              onChanged: (value){
-                /// 更新(刷新)
-                setState(() {
-                  flag = value;
-                });
-          }),
-          Text(
-              "同意和解协议",
-              style: TextStyle(
-                fontSize: 20,
-              ),)
+          Row(
+            children: <Widget>[
+              Text(title, style: textStyle1),
+            ],
+          ),
+          SizedBox(height: 8),
+          Text(desc, style: textStyle2),
+          SizedBox(height: 8),
+          Image.network(imageURL)
         ],
       ),
     );
   }
 }
+
+
+
+
