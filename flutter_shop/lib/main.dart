45fd8b29f0d0e07d95b8faa80f59662382ded1cb
Fearless <489368600@qq.com>
2020-07-24 18:13:43 +0800

    会员中心完成搭建

--- flutter_shop/lib/main.dart
+++ flutter_shop/lib/main.dart
@@ -1,27 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/config/app_theme.dart';
 import 'package:flutter_shop/routers/routers.dart';
-import 'config/index.dart';
 import 'provide/current_index_provide.dart';
 import 'package:provider/provider.dart';
 import 'provide/category_provide.dart';
 import 'provide/category_goods_list_provide.dart';
 import 'provide/detail_info_provide.dart';
 import 'provide/cart_provide.dart';
-
 import 'package:flutter_shop/routers/routes.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter_shop/routers/application.dart';
+import 'config/index.dart';
 
+/**
+ * Provide借助了InheritWidget，将共享状态放到顶层MaterialApp之上。底层部件通过Provier获取该状态
+ * 并且通过混合ChangeNotifier通知依赖于该状态的组件刷新
+ */
+
+/**
+ * 1、先创建自己需要共享的数据
+ * 2、在应用程序的顶层ChangeNotifierProvider
+ * 3、在其他位置使用共享的数据
+ * > Provider.of： 当Provider中的数据发生改变，Provider.of所在的Widget整个build方法都会重新构建
+ * > Consumer相对推荐：当Provider中的数据发生改变时，只会重新执行Consumer的builder
+ * > Selector<A, S>：
+ *   >> 1、selector方法作用：对原有的Provider数据进行转换，
+ *   >> 2、shouldRebuild作用：要不要重新构建
+ */
 void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => CurrentIndexProvide()),
-        ChangeNotifierProvider(create: (context) => CategoryProvider()),
-        ChangeNotifierProvider(create: (context) => CategoryGoodsListProvider()),
-        ChangeNotifierProvider(create: (context) => DetailsInfoProvide()),
-        ChangeNotifierProvider(create: (context) => CartProvide()),
+        ChangeNotifierProvider(create: (context) => CurrentIndexProvide(),),
+        ChangeNotifierProvider(create: (context) => CategoryProvider(),),
+        ChangeNotifierProvider(create: (context) => CategoryGoodsListProvider(),),
+        ChangeNotifierProvider(create: (context) => DetailsInfoProvide(),),
+        ChangeNotifierProvider(create: (context) => CartProvide(),),
       ],
       child: MyApp(),
     )
@@ -51,7 +65,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/**
- * Provide借助了InheritWidget，将共享状态放到顶层MaterialApp之上。底层部件通过Provier获取该状态
- * 并且通过混合ChangeNotifier通知依赖于该状态的组件刷新
- */
--- flutter_shop/lib/pages/member/member_page.dart
+++ flutter_shop/lib/pages/member/member_page.dart
@@ -1,16 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../config/index.dart';
 
-class MemberPage extends StatefulWidget {
-  @override
-  _MemberPageState createState() => _MemberPageState();
-}
-
-class _MemberPageState extends State<MemberPage> {
+class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("会员中心"),),
-      body: Center(),
+      body: ListView(
+        children: <Widget>[
+          _buildListViewHeaderWidget(),
+          _buildMyOrderWidget(),
+          _buildMyOrderTypeWidget(),
+          _buildActionList(),
+        ],
+      ),
     );
   }
+
+  /// headerView
+  Widget _buildListViewHeaderWidget() {
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20),
+      color: ZCColor.primaryColor,
+      child: Column(
+        children: <Widget>[
+          /// 头像
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            child: ClipOval(
+              child: SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset('assets/images/girl.jpeg', fit: BoxFit.cover,),
+              ),
+            ),
+          ),
+          /// 昵称
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              'Fearless',
+              style: TextStyle(
+                fontSize: ScreenUtil().setSp(36),
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// 我的订单
+  Widget _buildMyOrderWidget() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            width: 1,
+            color: ZCColor.defaultBorderColor,
+          ),
+        ),
+      ),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title: Text("我的订单"),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
+  /// 我的订单类型
+  Widget _buildMyOrderTypeWidget() {
+    return Container(
+      margin: EdgeInsets.only(top: 5),
+      width: ScreenUtil().setWidth(750),
+      height: ScreenUtil().setHeight(150),
+      padding: EdgeInsets.only(top: 20),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          /// 待付款
+          _buildSubButtonWidget(Icons.payment, "待付款"),
+          /// 待发货
+          _buildSubButtonWidget(Icons.directions_car, "待发货"),
+          /// 待收货
+          _buildSubButtonWidget(Icons.directions_car, "待收货"),
+          /// 待评价
+          _buildSubButtonWidget(Icons.message, "待评价"),
+        ],
+      ),
+    );
+  }
+  
+  Widget _buildSubButtonWidget(IconData imageData, String title) {
+    return Container(
+      width: ScreenUtil().setWidth(187),
+      child: Column(
+        children: <Widget>[
+          Icon(imageData, size: 30,),
+          Text(title),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionList() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+        children: <Widget>[
+          _buildMyListTile("领取优惠券"),
+          _buildMyListTile("已领取优惠券"),
+          _buildMyListTile("地址管理"),
+          _buildMyListTile("客服电话"),
+          _buildMyListTile("关于我们"),
+        ],
+      ),
+    );
+  }
+
+  /// cell
+  Widget _buildMyListTile(String title) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1, color: ZCColor.defaultBorderColor),
+        ),
+      ),
+      child: ListTile(
+        leading: Icon(Icons.blur_circular),
+        title: Text(title),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
 }
